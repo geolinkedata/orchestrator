e41d1c5eb8289c661ad261718d57469acc3bb2b5
Francesco Bartoli <francesco.bartoli@geobeyond.it>
2016-04-10 19:14:10 +0200

    Restart app from a template yo express-rest-api

--- server.js
+++ server.js
@@ -1,68 +1,11 @@
-var http = require('http'),
-    route = require('./routes'),
-    tasks = require('./tasks'),
-    port = require('./config.json').serverPort,
-    configTgeo = require('./config.json').tgeo,
-    timeout = require('./config.json').timeout,
-    express = require('express'),
-    path = require('path'),
-    logger = require('./utils/logger');
-    
+var settingsConfig = require('./app/config/settings/settings-config');
 
-
-
-var app = express();  
-app.set('port', port);
-
-//app.use(app.router);
-app.use(express.static(path.join(__dirname, '/public')));
-
-
-
-app.use(require('morgan')("combined", { "stream": logger.stream }));
-
-route.config(app);
-
-var tgeo = {
-    host: configTgeo.host,
-    port: configTgeo.port,
-    path: configTgeo.appUrl,
-    method: 'GET'
-};
-
-
-
-/*
-	http.createServer(app).listen(app.get('port'), function(){
-	    console.log('Express server listening on port ' + port);
-	});*/
-
-//checks if tgeo is running
-var req = http.request(tgeo, function(res){
-
-    res.on('data', function(chunk){
-       ;
-    });
-
-    res.on('end', function(err){
-        //http.createServer(routing.route).listen(port);
-	http.createServer(app).listen(app.get('port'), function(){
-	    console.log('Express server listening on port ' + port);
-	});
-	
-        setInterval(function(){
-            tasks.periodicHandler();
-        }, timeout);
-    });
-});
-
-
-
-req.on('error', function(e) {
-    console.log('problem with request: ' + e.message + '. Are tomcat and triplegeo running?');
-});
-
-req.end();
-
-
-
+if(settingsConfig.settings.clusterEnabled === 1) {
+  require('cluster-service').start({ workers: './app/config/worker-config.js',
+    accessKey: settingsConfig.settings.clusterAccessKey,
+    host: settingsConfig.settings.hostName,
+    port: settingsConfig.settings.masterPort });
+}
+else {
+  require('./app/config/worker-config.js');
+}
